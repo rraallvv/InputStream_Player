73532bb771bd3a4e3f92368771f54d597c44df7c
Developer <>
2018-04-26 23:37:35 -0400

    fix the input stream data source

--- app/src/main/java/com/example/amanmj/inputstream_player/InputStreamDataSource.java
+++ app/src/main/java/com/example/amanmj/inputstream_player/InputStreamDataSource.java
@@ -1,63 +1,105 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.amanmj.inputstream_player;
 
 import android.content.Context;
 import android.net.Uri;
-
+import com.google.android.exoplayer2.C;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DataSpec;
 
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
-/* my custom DataSource to read InputStream (Similar to AssetDataSource) */
-public class InputStreamDataSource implements DataSource {
+/**
+ * A {@link DataSource} for reading from a local asset.
+ */
+public final class InputStreamDataSource implements DataSource {
 
-	private Context context;
-	private FileInputStream inputStream;
+	private final Context context;
 	private Uri uri;
+	private InputStream inputStream;
 	private long bytesRemaining;
-	private boolean isOpen;
+	private boolean opened;
 
+	/**
+	 * @param context A context.
+	 */
 	public InputStreamDataSource(Context context) {
 		this.context = context;
 	}
 
 	@Override
-	public long open(DataSpec dataspec) throws IOException
-	{
-		uri = dataspec.uri;
+	public long open(DataSpec dataSpec) throws IOException {
+		uri = dataSpec.uri;
 
-		File file = new File(context.getCacheDir(),uri.toString());
+		File file = new File(context.getCacheDir(), uri.toString());
+		inputStream = new FileInputStream(file);
 
-		inputStream=new FileInputStream(file);
-
-		if (inputStream != null) {
-			isOpen = true;
-			bytesRemaining =  inputStream.available();
+		long skipped = inputStream.skip(dataSpec.position);
+		if (skipped < dataSpec.position) {
+			// assetManager.open() returns an AssetInputStream, whose skip() implementation only skips
+			// fewer bytes than requested if the skip is beyond the end of the asset's data.
+			throw new EOFException();
 		}
+		if (dataSpec.length != C.LENGTH_UNSET) {
+			bytesRemaining = dataSpec.length;
+		} else {
+			bytesRemaining = inputStream.available();
+			if (bytesRemaining == Integer.MAX_VALUE) {
+				// assetManager.open() returns an AssetInputStream, whose available() implementation
+				// returns Integer.MAX_VALUE if the remaining length is greater than (or equal to)
+				// Integer.MAX_VALUE. We don't know the true length in this case, so treat as unbounded.
+				bytesRemaining = C.LENGTH_UNSET;
+			}
+		}
+
+		opened = true;
 
 		return bytesRemaining;
 	}
 
 	@Override
-	public int read(byte[] buffer, int offset, int readLength) throws IOException
-	{
-		if (bytesRemaining == 0)
-		{
-			return -1;
+	public int read(byte[] buffer, int offset, int readLength) throws IOException {
+		if (readLength == 0) {
+			return 0;
+		} else if (bytesRemaining == 0) {
+			return C.RESULT_END_OF_INPUT;
 		}
-		else
-		{
-			int bytesRead = 0;
-			bytesRead = inputStream.read(buffer, offset, readLength);
 
-			if (bytesRead > 0) {
-				bytesRemaining -= bytesRead;
+		int bytesRead;
+		int bytesToRead = bytesRemaining == C.LENGTH_UNSET ? readLength
+				: (int) Math.min(bytesRemaining, readLength);
+		bytesRead = inputStream.read(buffer, offset, bytesToRead);
+
+		if (bytesRead == -1) {
+			if (bytesRemaining != C.LENGTH_UNSET) {
+				// End of stream reached having not read sufficient data.
+				throw new IOException();
 			}
-
-			return bytesRead;
+			return C.RESULT_END_OF_INPUT;
 		}
+		if (bytesRemaining != C.LENGTH_UNSET) {
+			bytesRemaining -= bytesRead;
+		}
+
+		return bytesRead;
 	}
 
 	@Override
@@ -65,29 +107,20 @@ public class InputStreamDataSource implements DataSource {
 		return uri;
 	}
 
-    @Override
-	public void close() throws IOException
-	{
+	@Override
+	public void close() throws IOException {
 		uri = null;
-		if (inputStream != null)
-		{
-			try
-			{
+		try {
+			if (inputStream != null) {
 				inputStream.close();
 			}
-			catch (IOException e)
-			{
-				throw new IOException(e);
-			}
-			finally
-			{
-				inputStream = null;
-				if (isOpen)
-				{
-					isOpen = false;
-				}
+		} catch (IOException e) {
+			throw new IOException();
+		} finally {
+			inputStream = null;
+			if (opened) {
+				opened = false;
 			}
 		}
 	}
 }
-
--- app/src/main/java/com/example/amanmj/inputstream_player/MainActivity.java
+++ app/src/main/java/com/example/amanmj/inputstream_player/MainActivity.java
@@ -55,12 +55,9 @@ public class MainActivity extends AppCompatActivity {
 			}
 		}
 
-		//DefaultRenderersFactory renderersFactorySound = new DefaultRenderersFactory(this,null, DefaultRenderersFactory.EXTENSION_RENDERER_MODE_OFF);
-		//player = ExoPlayerFactory.newSimpleInstance(renderersFactorySound, new DefaultTrackSelector(), new DefaultLoadControl());
 		player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
 
-		//final DataSource dataSource = new InputStreamDataSource(this);
-		final DataSource dataSource = new AssetDataSource(this);
+		final DataSource dataSource = new InputStreamDataSource(this);
 		final Uri uri = Uri.parse(filename);
 		DataSpec dataSpec = new DataSpec(uri);
 		try {
